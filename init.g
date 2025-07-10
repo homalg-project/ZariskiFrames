mohamed-barakat/CategoricalTowers

d&i CategoryOfAffineAlgebras

ZariskiFrames/init.g
@@ -4,6 +4,7 @@
 # Reading the declaration part of the package.
 #
 
+ReadPackage( "ZariskiFrames", "gap/AffineAlgebras.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrameOrCoframe.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrame.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiCoframe.gd");

ZariskiFrames/read.g
@@ -4,6 +4,7 @@
 # Reading the implementation part of the package.
 #
 
+ReadPackage( "ZariskiFrames", "gap/AffineAlgebras.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrameOrCoframe.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrame.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiCoframe.gi");

homalg-project/ZariskiFrames

init.g
@@ -4,6 +4,7 @@
 # Reading the declaration part of the package.
 #
 
+ReadPackage( "ZariskiFrames", "gap/CategoryOfAffineAlgebras.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrameOrCoframe.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrame.gd");
 ReadPackage( "ZariskiFrames", "gap/ZariskiCoframe.gd");

read.g
@@ -4,6 +4,7 @@
 # Reading the implementation part of the package.
 #
 
+ReadPackage( "ZariskiFrames", "gap/CategoryOfAffineAlgebras.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrameOrCoframe.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiFrame.gi");
 ReadPackage( "ZariskiFrames", "gap/ZariskiCoframe.gi");